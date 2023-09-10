ab25e3dcf798cbed3cf361053b126e4c85683818
Laurentiu <Laur8825@gmail.com>
2023-09-10 12:34:26 +0300

    Added operations

---
+++ src/Learning2.java
@@ -0,0 +1,14 @@
+public class Learning2 {
+    public static void main(String[] args) {
+        System.out.println("Feature");
+        double number1 = 10;
+        double number2 = 21;
+        System.out.println(number1 + number2);
+        System.out.println(number1 * number2);
+        System.out.println(number2 / number1);
+        System.out.println(number2 % number1);
+
+
+
+    }
+}
